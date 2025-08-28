1a102bbb222a2558e2552d9f9d5452604a67410e
Louis Billaut <ragedfear4875@gmail.com>
2025-08-28 21:50:20 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+document.addEventListener('mousemove', function(e) {
+    let main_rotation = document.getElementById('main-bg-image-blur');
+    let currentRotate = main_rotation.dataset.rotate ? parseFloat(main_rotation.dataset.rotate) : 0;
+
+    let x = e.clientX;
+    let y = e.clientY;
+    let dist = Math.hypot(x, y)
+
+    let newRotate = currentRotate + dist/1000;
+    
+    main_rotation.dataset.rotate = newRotate;
+    main_rotation.style.transform = `translate(-50%, -50%) rotate(${newRotate}deg) scale(1.44)`;
+});
