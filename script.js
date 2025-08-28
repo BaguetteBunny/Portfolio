47ad71f47f0d5b25d0fc64c5f5e6170018e90642
Louis Billaut <ragedfear4875@gmail.com>
2025-08-29 01:02:30 +0800

    add custom cursor

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-TITLES_ARRAY = "Creative Technologist"
+let currentX = 0;
+let currentY = 0;
+let movedX = 0;
+let movedY = 0;
+let customCursor = document.getElementById('custom-cursor');
+const delayFactor = 0.1;
 
 document.addEventListener('mousemove', function(e) {
-    let main_rotation = document.getElementById('main-bg-image-blur');
-    let currentRotate = main_rotation.dataset.rotate ? parseFloat(main_rotation.dataset.rotate) : 0;
+    let mainRotator = document.getElementById('main-bg-image-blur');
+    let currentRotate = mainRotator.dataset.rotate ? parseFloat(mainRotator.dataset.rotate) : 0;
 
-    let x = e.clientX;
-    let y = e.clientY;
-    let dist = Math.hypot(x, y)
+    movedX = e.clientX;
+    movedY = e.clientY;
+
+    let dist = Math.hypot(movedX, movedY);
 
     let newRotate = currentRotate + dist/1000;
     
-    main_rotation.dataset.rotate = newRotate;
-    main_rotation.style.transform = `translate(-50%, -50%) rotate(${newRotate}deg) scale(1.44)`;
+    mainRotator.dataset.rotate = newRotate;
+    mainRotator.style.transform = `translate(-50%, -50%) rotate(${newRotate}deg) scale(1.44)`;
 });
+
+function animateCursor() {
+    if (!customCursor) return;
+    currentX += (movedX - currentX) * delayFactor;
+    currentY += (movedY - currentY) * delayFactor;
+
+    customCursor.style.left = `${currentX}px`;
+    customCursor.style.top = `${currentY}px`;
+
+    requestAnimationFrame(animateCursor);
+}
+
+animateCursor();
